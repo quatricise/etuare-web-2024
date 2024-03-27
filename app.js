25cbf51b174530c670b93c61330ca75bc33c4f59
quatricise <stepantrvaj@gmail.com>
2024-03-27 20:13:10 +0100

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+//search query parsing
+function init() {
+
+  /* Process the URL search */
+  let searchQuery = window.location.search.replace("?", "")
+  let pairs = searchQuery.split("+")
+  pairs.forEach(pair => {
+    let [key, value] = pair.split("=")
+    
+    switch(key) {
+      case "project": {
+        //display a specific project
+        break
+      }
+    }
+  })
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# ETUARE
+
+### Stránky pro webové studio etuare, založené praotcem Etuare v roce 1969, kdy se se mu narodil jeho syn - Zbyněk Etuare
